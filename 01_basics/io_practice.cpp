e2e2a323ca8aabaf3674a60d04142ac41baf09cd
jasonbyurn-phy <kemi123@korea.ac.kr>
2026-08-13 19:41:27 +0900

    feat: while문 연습 (1.4.1)

--- 01_basics/io_practice.cpp
+++ 01_basics/io_practice.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-
+/*
+* Simple main function
+* read two numbers and write their sum
+*/
 int main()
 {
+    // prompt user to enter two numbers
     std::cout << "Enter two number:" << std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
-    std::cout << "The sum of" << v1 << " and " << v2
+    int v1 = 0, v2 = 0; // variables to hold the input we read
+    std::cin >> v1 >> v2; // read input
+    std::cout << "The sum of " << v1 << " and " << v2
     << " is " << v1 + v2 << std::endl;
     return 0;
 }
