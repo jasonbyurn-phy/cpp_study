512cb54610946ae3719b3f14ccd8eee05eb5a05b
jasonbyurn-phy <kemi123@korea.ac.kr>
2026-08-31 18:37:57 +0900

    feat : Sales_data.h 헤더 가드 작성 및 실습 완료 (2.6.3, ch2 완료)

--- 02_variables/Sales_data.cpp
+++ 02_variables/Sales_data.cpp
@@ -1,10 +1,3 @@
-Struct Sales_data {
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
-};
-Sales_data accum, trans, *salesptr;
-
 #include <iostream>
 #include <string>
 #include "Sales_data.h"
