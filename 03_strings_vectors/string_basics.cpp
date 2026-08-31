252023f1a3e7ec5d9b5ccd8ae806e3b8ce844bec
jasonbyurn-phy <kemi123@korea.ac.kr>
2026-08-31 20:24:17 +0900

    feat: string 기본 (3.2.2)

---
+++ 03_strings_vectors/string_basics.cpp
@@ -0,0 +1,32 @@
+#include <string>
+using std::string;
+
+string s1; // default initialization; s1 is the empty string
+string s2 = s1; // s2 is a copy of s1
+string s3 = "hiya"; // s3 is a copy of the string literal
+string s4(10, 'c'); // s4 is cccccccccc
+
+string s5 = "hiya"; // copy initialization
+string s6("hiya"); // directi initialization
+string s7(10, 'c'); // directi initialization; s7 is cccccccccc
+string s8 = string(10, 'c'); // copy initialization
+
+#include <iostream>
+using std::cin, using std::cout;
+int main()
+{
+    // string s; // empty string
+    // cin >> s; // read a whitespace-separated string into s
+    // cout << s << endl; // write s to the output
+    // return 0;
+
+    // string s1, s2;
+    // cin >> s1 >> s2; // read first input into s1, second into s2
+    // cout << s1 << s2 << endl; // write both strings
+    // return 0;
+
+    string word;
+    whild (cin >> word) // read until end-of-file
+    cout << word << endl; // write each word followd by a new line
+    return 0;
+}
